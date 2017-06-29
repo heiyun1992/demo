ea5f08f775793a821604a7f848437147c8ec0f40
heiyun1992 <1946314801@qq.com>
2017-06-29 15:56:12 +0800

    定时任务

---
+++ src/main/java/com/example/task/Scheduler2Task.java
@@ -0,0 +1,22 @@
+package com.example.task;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by maozz11347 on 2017/6/27.
+ */
+@Component
+public class Scheduler2Task {
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 6000)
+    public void reportCurrentTime() {
+        System.out.println("现在时间：" + dateFormat.format(new Date()));
+    }
+
+}
